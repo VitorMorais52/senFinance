48e3ad6b09fb4125b8c5ee2571687ffc4a4934c5
Morais <vitormorais52@gmail.com>
2022-03-19 23:27:49 -0300

    improve style TransactionsTable component

--- src/components/common/TransactionsTable/index.tsx
+++ src/components/common/TransactionsTable/index.tsx
@@ -36,69 +36,80 @@ function TransactionsTable() {
   }
 
   return (
-    <div className="container-transactions-table">
-      <FiltersModal
-        isOpen={isFiltersModalOpen}
-        onRequestClose={handleCloseFilterModal}
-      />
-      <div className="filters">
-        <button
-          type="button"
-          className="button-show-filter"
-          onClick={handleOpenFilterModal}
-        >
-          <span>Filtros</span>
-        </button>
-      </div>
+    <>
+      {filteredTransactions.length > 0 ? (
+        <div className="container-transactions-table">
+          <FiltersModal
+            isOpen={isFiltersModalOpen}
+            onRequestClose={handleCloseFilterModal}
+          />
+          <div className="filters">
+            <button
+              type="button"
+              className="button-show-filter"
+              onClick={handleOpenFilterModal}
+            >
+              <span>Filtros</span>
+            </button>
+          </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Titulo</th>
-            <th>Valor</th>
-            <th>Categoria</th>
-            <th>Data</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredTransactions.map((transaction) => (
-            <tr key={transaction.id}>
-              <td>{transaction.title}</td>
-              <td className={transaction.type + "-table"}>
-                {parseToCurrency(transaction.amount)}
-              </td>
-              <td>{transaction.category}</td>
-              <td>{dateFormat(transaction.createdAt)}</td>
-              <td>
-                <button
-                  type="button"
-                  className="button-handle-transaction"
-                  onClick={() => handleEditTransaction(transaction)}
-                >
-                  <img
-                    className="img-button-transaction"
-                    src={editIcon}
-                    alt="Edit transaction"
-                  />
-                </button>
-                <button
-                  type="button"
-                  className="button-handle-transaction"
-                  onClick={() => removeTransaction(transaction.id)}
-                >
-                  <img
-                    className="img-button-transaction"
-                    src={deleteIcon}
-                    alt="Delete transaction"
-                  />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Titulo</th>
+                <th>Valor</th>
+                <th>Categoria</th>
+                <th>Data</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredTransactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td>{transaction.title}</td>
+                  <td className={transaction.type + "-table"}>
+                    {parseToCurrency(transaction.amount)}
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{dateFormat(transaction.createdAt)}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="button-handle-transaction"
+                      onClick={() => handleEditTransaction(transaction)}
+                    >
+                      <img
+                        className="img-button-transaction"
+                        src={editIcon}
+                        alt="Edit transaction"
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      className="button-handle-transaction"
+                      onClick={() => removeTransaction(transaction.id)}
+                    >
+                      <img
+                        className="img-button-transaction"
+                        src={deleteIcon}
+                        alt="Delete transaction"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="not-results">
+          <h4>Nenhum registro encontrado.</h4>
+          <span className="span-not-results">
+            Registre sua primeira transação clicando em <b>Nova transação</b>
+          </span>
+        </div>
+      )}
+    </>
   );
 }
 
